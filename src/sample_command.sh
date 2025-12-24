3b94da794e8fa94526345e62296fd5a189df2771
Noah Krieger <noahkrieger@gmail.com>
2025-12-24 09:34:25 -0500

    wip

--- src/sample_command.sh
+++ src/sample_command.sh
@@ -1,7 +1,7 @@
 ./experiments/runner.py \
---bucket-name-prefix experiment-2-20251214 \
+--bucket-name-prefix experiment-2-20251218 \
 --trial-lengths-minutes 5 \
 --users 10 20 30 40 50 60 70 80 \
 --trial-numbers -60 \
 --host http://globeco.local:32080 \
---resource-profile default 
+--resource-profile default-2 
