746df8ed056a57744be6804830d4e1309dbea1be
Noah Krieger <noahkrieger@gmail.com>
2026-01-12 15:18:56 -0500

    wip

--- src/experiment_3.sh
+++ src/experiment_3.sh
@@ -5,6 +5,5 @@
 --trial-numbers -60 \
 --host http://globeco.local:32080 \
 --resource-profile configuration-3 configuration-4 configuration-5 configuration-6 \
---wait-for-cooling-off
 --wait-for-cooling
 
