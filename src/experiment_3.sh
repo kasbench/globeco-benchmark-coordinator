37d5aff11a459a9e3ee03e48d62b3bd12f2fe2e1
Noah Krieger <noahkrieger@gmail.com>
2026-01-14 13:00:46 -0500

    replaced configurations

--- src/experiment_3.sh
+++ src/experiment_3.sh
@@ -2,8 +2,8 @@
 --bucket-name-prefix experiment-3-20260113 \
 --trial-lengths-minutes 5 \
 --users 40 \
---trial-numbers -60 \
+--trial-numbers -30 \
 --host http://globeco.local:32080 \
---resource-profiles default-2 \
+--resource-profiles configuration-1 configuration-2 configuration-3 configuration-4 configuration-5 configuration-6 configuration-7 configuration-8 configuration-9 configuration-10 configuration-11 configuration-12  \
 --wait-for-cooling
 
