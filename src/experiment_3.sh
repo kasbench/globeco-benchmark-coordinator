780afb037127ee919218df36120218ff852b2029
Noah Krieger <noahkrieger@gmail.com>
2026-01-14 12:52:41 -0500

    wip

--- src/experiment_3.sh
+++ src/experiment_3.sh
@@ -1,9 +1,9 @@
 ./experiments/runner.py \
---bucket-name-prefix experiment-3-20260107 \
+--bucket-name-prefix experiment-3-20260113 \
 --trial-lengths-minutes 5 \
 --users 40 \
---trial-numbers -30 \
+--trial-numbers -60 \
 --host http://globeco.local:32080 \
---resource-profile configuration-3 configuration-4 configuration-5 configuration-6 configuration-7 configuration-8 configuration-9 configuration-10 configuration-11 configuration-12 configuration-13 configuration-14 configuration-15 configuration-16 configuration-17 \
+--resource-profiles default-2 \
 --wait-for-cooling
 
