164328341276cc70456fef6b72d846a5f66300c8
Noah Krieger <noahkrieger@gmail.com>
2026-01-17 21:29:57 -0500

    wip

--- src/experiment_3_default.sh
+++ src/experiment_3_default.sh
@@ -2,7 +2,7 @@
 --bucket-name-prefix experiment-3-20260117 \
 --trial-lengths-minutes 5 \
 --users 40 \
---trial-numbers -60 \
+--trial-numbers -120 \
 --host http://globeco.local:32080 \
 --resource-profiles default-2  \
 --wait-for-cooling
