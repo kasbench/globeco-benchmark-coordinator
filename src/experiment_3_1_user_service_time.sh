f28cab4efc13e613ef77c0bcaa87e079de0571e6
Noah Krieger <noahkrieger@gmail.com>
2026-01-23 10:25:17 -0500

    added shell script to collect service times

---
+++ src/experiment_3_1_user_service_time.sh
@@ -0,0 +1,9 @@
+./experiments/runner.py \
+--bucket-name-prefix experiment-3-20260117 \
+--trial-lengths-minutes 5 \
+--users 1 \
+--trial-numbers -60 \
+--host http://globeco.local:32080 \
+--resource-profiles configuration-10-100-False \
+--wait-for-cooling
+
