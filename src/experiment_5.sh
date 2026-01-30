48ab7ba763b8f3cdf9dddd93554260fd56916d1c
Noah Krieger <noahkrieger@gmail.com>
2026-01-29 20:34:53 -0500

    wip

--- src/experiment_5.sh
+++ src/experiment_5.sh
@@ -4,8 +4,8 @@
 --users 40 \
 --trial-numbers -60 \
 --host http://globeco.local:32080 \
---resource-profiles configuration_nl-20-000-True configuration_nl-20-025-True\
-configuration_nl-20-050-True configuration_nl-20-075-True configuration_nl-20-100-True  \
+--resource-profiles configuration_nl-20-000-True configuration_nl-20-025-True \
+ configuration_nl-20-050-True configuration_nl-20-075-True configuration_nl-20-100-True  \
 --replicas 1  \
 --wait-for-cooling
 
